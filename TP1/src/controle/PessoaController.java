64c3f68ffda17fc3b1915ae0efe0bdbfd025201c
DesenvolvedorJava <DesenvolvedorJava@BlocoDesenvolvimentoJava-PC.localdomain>
2018-10-25 12:49:42 -0200

    Initial

---
+++ TP1/src/controle/PessoaController.java
@@ -0,0 +1,21 @@
+package controle;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PessoaController extends HttpServlet{
+	
+	public PessoaController() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		
+	}
+	
+
+}
