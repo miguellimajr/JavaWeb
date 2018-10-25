88b0f538e3cb9ad7dfa1f8afb08dab2bd9c73bee
DesenvolvedorJava <DesenvolvedorJava@BlocoDesenvolvimentoJava-PC.localdomain>
2018-10-25 13:20:37 -0200

    2

--- TP1/src/controle/PessoaController.java
+++ TP1/src/controle/PessoaController.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import negocio.Pessoa;
+
 public class PessoaController extends HttpServlet{
 	
 	public PessoaController() {
@@ -14,7 +16,13 @@ public class PessoaController extends HttpServlet{
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		
+		Pessoa pessoa = new Pessoa(
+				request.getParameter("nome"),
+				request.getParameter("email"),
+				request.getParameter("senha")
+				);
+			
+				
 	}
 	
 
